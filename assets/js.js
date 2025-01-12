54d79f98386f47c90ae3adc23f1466f950d351a2
mhammed2008 <79751749+mhammed2008@users.noreply.github.com>
2025-01-12 12:57:52 +0200

    DevTrack: Initial commit with existing project files

---
+++ assets/js.js
@@ -0,0 +1,12 @@
+
+let nav = document.getElementById('nav');
+window.onscroll = function () { scrollNav() };
+
+function scrollNav() {
+    if (window.scrollY > 30) {
+        nav.classList.remove('py-4');
+    } else {
+        nav.classList.add('py-4');
+    }
+}
+
